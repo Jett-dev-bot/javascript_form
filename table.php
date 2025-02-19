6fed006d97756bb96cda9eaec0326fd88c9036ad
Jett <jsumatra23-0289@cca.edu.ph>
2025-02-19 19:50:04 +0800

    v

--- table.php
+++ table.php
@@ -7,6 +7,11 @@
     <table id="student_record">
     
     </table>
-    
+     <script>
+        const studentRecord = document.getElementById("student_record");
+
+        console.log("Hello World");
+        console.log(studentRecord);
+     </script>
 </body>
 </html>
